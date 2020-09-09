09317e1c570cce1d4eef8e5a9b4aeeb7018802d5
장혜린 <64074770+hyerin1105@users.noreply.github.com>
2020-09-09 21:53:51 +0900

    2week_rest

--- 2week_rest/post/serializer.py
+++ 2week_rest/post/serializer.py
@@ -9,3 +9,4 @@ class PostSerializer(serializers.ModelSerializer):
         read_only_fields = ('title',)
         # title의 변경을 허용하지 않을 때.(나말고는 title 변경 ㄴㄴ)
         # read 말고 write로 써도 무방.
+        
