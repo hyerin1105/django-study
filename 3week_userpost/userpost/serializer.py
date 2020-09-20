ffe3a53ed887fa4e900178e6e1335593549ed9ba
장혜린 <64074770+hyerin1105@users.noreply.github.com>
2020-09-21 00:34:48 +0900

    Delete serializer.py

--- 3week_userpost/userpost/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import UserPost
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserPost
-        fields = '__all__'
